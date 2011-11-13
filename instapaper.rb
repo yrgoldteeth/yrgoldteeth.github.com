cd5c7ac85ae6ed2e4f60fcfb7753177861101f64
Nicholas Fine <nicholas.fine@gmail.com>
2011-11-12 18:16:27 -0600

    update instapaper links

--- instapaper.rb
+++ instapaper.rb
@@ -24,14 +24,14 @@ class Instapaper
     pub_date = Time.parse(link['pubDate']).strftime('%b %e %Y')
     href = link['link']
     title = link['title']
-    "<li class='post-link'><span class='label'>#{pub_date}</span><br /><a href='#{href}'>#{title}</a></li>"
+    "    <li class='post-link'><span class='label'>#{pub_date}</span><br /><a href='#{href}'>#{title}</a></li>"
   end
 
   def write_links_to_file
     `cp ./.instapaper/instapaper.markdown .`
     markdown_file = File.open(PAGE_FILE, 'a')
     links.each{|link| markdown_file.puts(generate_li(link));save_link(link)}
-    markdown_file.puts('</ul>')
+    markdown_file.puts('  </ul>')
     markdown_file.puts('</div>')
   end
 
@@ -63,18 +63,17 @@ class Instapaper
   end
 end
 
-class Pusher
-  def initialize
-    cmd = "cd /home/ubuntu/code/yrgoldteeth.github.com && git add . && git add ./instapaper.markdown && "
-    cmd += "git commit -m'update instapaper favorites' && git push origin master"
-    `#{cmd}`
-  end
-end
+#class Pusher
+#  def initialize
+#    cmd = "cd /home/ubuntu/code/yrgoldteeth.github.com && git add . && git add ./instapaper.markdown && "
+#    cmd += "git commit -m'update instapaper favorites' && git push origin master"
+#    `#{cmd}`
+#  end
+#end
 
 if File.exist?('./.instapaper/config')
   instapaper_url = File.readlines('./.instapaper/config').map(&:strip).first
   `wget #{instapaper_url} -O ipaper.rss`
   i = Instapaper.new('./ipaper.rss', true)
   i.run
-  Pusher.new if i.new_links? && i.complete
 end
